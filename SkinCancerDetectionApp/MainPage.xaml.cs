482557a391859073357851b6ff5cd7a19be9ca05
Tomas3371~ <Tomasdavid337@seznam.cz>
2025-05-04 16:31:41 +0200

    feat: initial permission handling

--- SkinCancerDetectionApp/MainPage.xaml.cs
+++ SkinCancerDetectionApp/MainPage.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace SkinCancerDetectionApp;
+﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Maui.Controls;
+
+namespace SkinCancerDetectionApp;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+
+        _blazorWebView.BlazorWebViewInitializing += BlazorWebViewInitializing;
+        _blazorWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
+    }
+
+    private partial void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e);
+    private partial void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e);
 }
---
+++ SkinCancerDetectionApp/Platforms/Windows/IPermissionRequestHandler.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Web.WebView2.Core;
+
+namespace SkinCancerDetectionApp.Platforms.Windows;
+
+internal interface IPermissionRequestHandler
+{
+    void OnPermissionRequested(CoreWebView2 sender, CoreWebView2PermissionRequestedEventArgs args);
+}
